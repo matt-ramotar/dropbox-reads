424b7b589198940cb2939d62076ecdd88f8e9aea
Matt Ramotar <mramotar@dropbox.com>
2021-07-04 22:22:10 -0400

    Add Google login

--- src/pages/Splash/Splash.tsx
+++ src/pages/Splash/Splash.tsx
@@ -1,9 +1,20 @@
 import { Box, Button, Typography } from "@material-ui/core";
+import GoogleLogin, {
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+} from "react-google-login";
 import logo from "../../images/logo.png";
 import splash from "../../images/splash.png";
+import { GOOGLE_CLIENT_ID } from "../../util/secrets";
 import styles from "./splash.module.scss";
 
 export default function Splash(): JSX.Element {
+  const handleLogin = async (
+    data: GoogleLoginResponse | GoogleLoginResponseOffline
+  ) => {
+    console.log(data);
+  };
+
   return (
     <Box>
       <Typography>Dropbox Reads</Typography>
@@ -21,6 +32,14 @@ export default function Splash(): JSX.Element {
           <Typography>Sign Up</Typography>
         </Button>
       </Box>
+
+      <GoogleLogin
+        clientId={GOOGLE_CLIENT_ID!}
+        buttonText="Log in with Google"
+        onSuccess={handleLogin}
+        onFailure={handleLogin}
+        cookiePolicy={"single_host_origin"}
+      />
     </Box>
   );
 }
--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -1,1 +1,2 @@
 export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
