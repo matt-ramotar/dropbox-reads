873fb296bbb13fe19ab44ea8128d430859a7a6d5
Matt Ramotar <mramotar@dropbox.com>
2021-07-05 11:37:59 -0400

    Handle login

--- src/pages/Splash/Splash.tsx
+++ src/pages/Splash/Splash.tsx
@@ -3,16 +3,49 @@ import GoogleLogin, {
   GoogleLoginResponse,
   GoogleLoginResponseOffline,
 } from "react-google-login";
+import { extractUsername } from "../../helpers";
 import logo from "../../images/logo.png";
 import splash from "../../images/splash.png";
+import { continueWithGoogle } from "../../lib";
 import { GOOGLE_CLIENT_ID } from "../../util/secrets";
 import styles from "./splash.module.scss";
+
+interface ProfileObj {
+  googleId: string;
+  imageUrl: string;
+  email: string;
+  name: string;
+  givenName: string;
+  familyName: string;
+}
 
 export default function Splash(): JSX.Element {
   const handleLogin = async (
     data: GoogleLoginResponse | GoogleLoginResponseOffline
   ) => {
-    console.log(data);
+    if ("code" in data) return;
+
+    const {
+      givenName: firstName,
+      familyName: lastName,
+      email,
+      googleId,
+      imageUrl: picture,
+    } = data.profileObj as ProfileObj;
+
+    const username = extractUsername(email);
+
+    const { user, token } = await continueWithGoogle(
+      firstName,
+      lastName,
+      email,
+      username,
+      googleId,
+      picture
+    );
+
+    console.log("USER", user);
+    console.log("TOKEN", token);
   };
 
   return (
