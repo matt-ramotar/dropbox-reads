71b4e04f40fddffef3e857a18bc709260f1b4534
Matt Ramotar <mramotar@dropbox.com>
2021-07-05 16:15:08 -0400

    Refactor Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,13 @@
 import { Grid, Typography } from "@material-ui/core";
+import SafeUser from "../../types/SafeUser";
 import styles from "./home.module.scss";
 
-export default function Home(props: any): JSX.Element {
-  console.log(props);
-
+export default function Home({ user }: { user: SafeUser }): JSX.Element {
   return (
     <Grid className={styles.grid}>
       <Typography>Home</Typography>
+      <Typography>{`Hey, ${user.firstName} ${user.lastName}!`}</Typography>
+      <Typography>{user.username}</Typography>
     </Grid>
   );
 }
