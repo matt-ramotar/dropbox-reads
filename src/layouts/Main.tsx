d4cb46de2afb13b6c04be351fec2422942491425
Matt Ramotar <mramotar@dropbox.com>
2021-07-05 14:31:57 -0400

    Add Main layout

---
+++ src/layouts/Main.tsx
@@ -0,0 +1,6 @@
+import { Grid } from "@material-ui/core";
+import styles from "../sass/Main.module.scss";
+
+export default function Main(): JSX.Element {
+  return <Grid className={styles.grid}></Grid>;
+}
