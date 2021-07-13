28278556a954d17eaed65a70ced35dfbce24112e
Miguel Michel <mmichel@dropbox.com>
2021-07-12 17:32:03 -0700

    create page with form to add a book recommendation

---
+++ src/pages/AddBook/AddBook.tsx
@@ -0,0 +1,14 @@
+import { Grid, Typography } from "@material-ui/core";
+import SafeUser from "../../types/SafeUser";
+import styles from "./AddBook.module.scss";
+import AddBookForm from "../../components/AddBookForm";
+
+export default function Explore({ user }: { user: SafeUser }): JSX.Element {
+    return (
+        <Grid className={styles.grid} >
+            <Typography>{`Hey, ${user.firstName} ${user.lastName}!`}</Typography>
+            <Typography>What book would you like to recommend?</Typography>
+            <AddBookForm />
+        </Grid>
+    );
+}
