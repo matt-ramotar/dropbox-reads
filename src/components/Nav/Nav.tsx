b4eabcd7577f817161ac8a9b7ce5c39e0eddfbef
Miguel Michel <mmichel@dropbox.com>
2021-07-12 17:30:27 -0700

    add button that routes to /add-book

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,5 @@
-import { Grid } from "@material-ui/core";
 import React from "react";
+import { Grid, Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
 import SafeUser from "../../types/SafeUser";
@@ -14,6 +14,9 @@ export default function Nav(props: Props): JSX.Element {
     <Grid container className={styles.root}>
       <Link to="/">
         <img src={logo} alt="Dropbox Reads logo" />
+      </Link>
+      <Link to="/add-book">
+        <Button variant="outlined" size="large" color="primary">Add Book</Button>
       </Link>
     </Grid>
   );
