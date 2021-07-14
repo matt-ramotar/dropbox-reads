d38f4652d0f14dbe7b7daa9a901d5ad5fd070b03
Miguel Michel <mmichel@dropbox.com>
2021-07-13 20:26:35 -0700

    add google books api url to endpoints constants

--- src/util/endpoints.ts
+++ src/util/endpoints.ts
@@ -2,3 +2,4 @@ export const VERSION = "/v1";
 
 export const CONTINUE_WITH_GOOGLE = "/auth/continue/google";
 export const VALIDATE_TOKEN = "/auth/token/validate";
+export const GOOGLE_BOOKS_URL = "https://www.googleapis.com/books/v1/volumes?q="
