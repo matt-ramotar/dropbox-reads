2f21e8fb9f026df7336327362ec42d432fc09137
angela chan <achan@dropbox.com>
2021-07-14 15:16:45 -0700

    add book detail route
    add bookid routes
    
    set up book details page, make summary left sidebar, and set up basics for comments section
    
    add in upvotes
    
    add upvotes
    
    add fetch godbook endpoint and replace hardcoded book details

--- src/util/endpoints.ts
+++ src/util/endpoints.ts
@@ -5,5 +5,6 @@ export const VALIDATE_TOKEN = "/auth/token/validate";
 
 export const GET_TAGS = "/tags";
 export const GET_BOOKS = "/books";
+export const GET_GOD_BOOK = "/books/{bookId}/god";
 
 export const GOOGLE_BOOKS_URL = "https://www.googleapis.com/books/v1/volumes?q="
