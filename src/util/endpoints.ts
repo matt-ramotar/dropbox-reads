e4597b2b8aa869604ca01263da9a95fa31f0cf37
Matt Ramotar <mramotar@dropbox.com>
2021-07-15 11:15:13 -0400

    Add endpoints

--- src/util/endpoints.ts
+++ src/util/endpoints.ts
@@ -2,3 +2,6 @@ export const VERSION = "/v1";
 
 export const CONTINUE_WITH_GOOGLE = "/auth/continue/google";
 export const VALIDATE_TOKEN = "/auth/token/validate";
+
+export const GET_TAGS = "/tags";
+export const GET_BOOKS = "/books";
