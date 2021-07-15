afe59822995c54d25dd7cfdfc49762d642ca8c74
Brandon Seamster <bseamster@dropbox.com>
2021-07-15 13:13:59 -0700

    add debounce to search bar and prevent auto search if query is empty

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,32 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { Toolbar, AppBar, InputBase, IconButton } from "@material-ui/core";
+import { useMemo } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
 import { fetchByTags } from "../../lib";
-
+import _ from "lodash";
 
 export default function SearchBar(): JSX.Element {
-  const [data, setData] = useState({})
-  const [query, setQuery] = useState('')
+  const [data, setData] = useState({});
+  const [query, setQuery] = useState("");
 
   // todo: use debouncer
+  const changeHandler = (e: React.ChangeEvent<any>) => {
+    setQuery(e.target.value);
+  };
+  const debouncedChangeHandler = useMemo(() => _.debounce(changeHandler, 600), []);
+
   useEffect(() => {
-    let response
+    let response;
     try {
-      response = fetchByTags(query)
-      console.log(response)
-      setData(response)
+      if (query) {
+        response = fetchByTags(query);
+        console.log(response);
+        setData(response);
+      }
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }, [query])
+  }, [query]);
   return (
     <>
       <Toolbar>
         <InputBase
           placeholder="Search recommendations…"
-          inputProps={{ 'aria-label': 'search' }}
-          onChange={(e) => setQuery(e.target.value)}
+          inputProps={{ "aria-label": "search" }}
+          onChange={debouncedChangeHandler}
         />
       </Toolbar>
     </>
