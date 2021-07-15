a33dac18132399311e3d2367b67b8395b8943ffc
Matt Ramotar <mramotar@dropbox.com>
2021-07-15 11:12:13 -0400

    Refactor routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import { StylesProvider, ThemeProvider } from "@material-ui/core/styles";
 import { useEffect, useState } from "react";
 import { useRoutes } from "react-router";
 import "./App.scss";
+import fetchBooks from "./lib/fetchBooks";
+import fetchTags from "./lib/fetchTags";
 import validateToken, { ValidateTokenSuccess } from "./lib/validateToken";
 import routes from "./routes.js";
 import theme from "./theme";
+import { GodBook } from "./types/GodBook";
 import SafeUser from "./types/SafeUser";
+import { Tag } from "./types/Tag";
 
-function App({ user }: { user: SafeUser | null }): JSX.Element | null {
+function App({ user, tags, books }: { user: SafeUser | null, tags: Tag[], books: GodBook[] }): JSX.Element | null {
   const isLoggedIn = Boolean(user);
 
-  const routing = useRoutes(routes(isLoggedIn, user));
+  const routing = useRoutes(routes(isLoggedIn, user, tags, books));
 
   return (
     <ThemeProvider theme={theme}>
@@ -22,6 +26,8 @@ function App({ user }: { user: SafeUser | null }): JSX.Element | null {
 export default function AppHoc(): JSX.Element | null {
   const [isValidated, setIsValidated] = useState<boolean | null>(null);
   const [safeUser, setSafeUser] = useState<SafeUser | null>(null);
+  const [tags, setTags] = useState<Tag[] | null>(null)
+  const [books, setBooks] = useState<GodBook[] | null>(null)
 
   useEffect(() => {
     const localToken = localStorage.getItem("TOKEN");
@@ -40,6 +46,25 @@ export default function AppHoc(): JSX.Element | null {
     }
   }, [setIsValidated]);
 
-  if (isValidated === null) return null;
-  return <App user={safeUser} />;
+  useEffect(() => {
+    async function loadTagsAsync() {
+      const tagsResponse = await fetchTags()
+      setTags(tagsResponse)
+    }
+
+    loadTagsAsync()
+  }, [setTags])
+
+  useEffect(() => {
+    async function loadBooksAsync() {
+      const booksResponse = await fetchBooks()
+      setBooks(booksResponse)
+    }
+
+    loadBooksAsync()
+
+  }, [setBooks])
+
+  if (isValidated === null || tags === null || books === null) return null;
+  return <App user={safeUser} tags={tags} books={books}/>;
 }
