74ef4381158e2203030d99eb1f4d6c4a49affe09
Matt Ramotar <mramotar@dropbox.com>
2021-07-15 11:15:07 -0400

    Add Filters slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import authReducer from "./auth";
+import filtersSlice from "./filters";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    filters: filtersSlice,
   },
   middleware: () => getDefaultMiddleware().concat(logger),
   devTools: process.env.REACT_APP_ENV !== "production",
