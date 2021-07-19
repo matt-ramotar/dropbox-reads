2ad8965d90a4e61def3c25fc96d41661841a27a3
Matt Ramotar <mramotar@dropbox.com>
2021-07-18 20:58:03 -0400

    Add src error handling

--- src/components/cards/actions/FollowUserActionCard/FollowUserActionCard.tsx
+++ src/components/cards/actions/FollowUserActionCard/FollowUserActionCard.tsx
@@ -1,7 +1,9 @@
 import { Box, Button, Grid, Typography } from "@material-ui/core";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { GodAction } from "../../../../types/GodAction";
 import { GodUser } from "../../../../types/GodUser";
+import { DefaultAvatar } from "../../../../util/defaults";
 import styles from "./FollowUserActionCard.module.scss";
 
 interface Props {
@@ -11,12 +13,18 @@ interface Props {
 }
 
 export default function FollowUserActionCard(props: Props): JSX.Element | null {
+  const [imageSrc, setImageSrc] = useState(props.user.picture);
+  const [otherUserImageSrc, setOtherUserImageSrc] = useState(props.otherUser.picture);
+
+  const onErrorOtherUser = () => setOtherUserImageSrc(DefaultAvatar);
+  const onError = () => setImageSrc(DefaultAvatar);
+
   return (
     <Grid className={styles.root}>
       <Box className={styles.about_container}>
         <Grid className={styles.about}>
           <Box className={styles.avatar}>
-            <img src={props.user.picture} alt={props.user.username} className={styles.avatar} />
+            <img src={imageSrc!} onError={onError} alt={props.user.username} className={styles.avatar} />
           </Box>
           <Box className={styles.header}>
             <Link to={`/${props.user.username}`}>
@@ -37,7 +45,7 @@ export default function FollowUserActionCard(props: Props): JSX.Element | null {
 
       <Box className={styles.main}>
         <Box className={styles.user_container}>
-          <img src={props.otherUser.picture} alt={props.otherUser.username} className={styles.avatar} />
+          <img src={otherUserImageSrc!} onError={onErrorOtherUser} alt={props.otherUser.username} className={styles.avatar} />
 
           <Box className={styles.otherUserInfo}>
             <Typography className={styles.text_dynamic}>{`${props.otherUser.firstName} ${props.otherUser.lastName}`}</Typography>
